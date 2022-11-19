789c46065d87fe8302704123bc9706ea1fc575e8
Shivam Menda <shivammenda2002@gmail.com>
2022-11-19 18:32:17 +0530

    urls setup.

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse 
+def index(request):
+    return HttpResponse("Index Page")
+def posts(request):
+    return HttpResponse("Posts page")
+def post_detail(request):
+    return HttpResponse("Post detail page")

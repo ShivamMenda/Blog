4057719fe977c4c4fbc6e0780f383f2a8da774fd
Shivam Menda <shivammenda2002@gmail.com>
2022-11-20 16:18:54 +0530

    All pages setup.

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 def index(request):
-   return render(request,'blog/index.html')
+    return render(request,'blog/index.html')
 def posts(request):
-    return HttpResponse("Posts page")
-def post_detail(request):
-    return HttpResponse("Post detail page")
+    return render(request,'blog/all-posts.html')
+def post_detail(request,slug):
+    return render(request,"blog/post-detail.html")

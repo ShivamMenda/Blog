8a3c61e5feff41dd7bb223074ccbda84c8031a03
Shivam Menda <shivammenda2002@gmail.com>
2022-11-20 15:04:07 +0530

    Home Page with css setup.

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 def index(request):
-    return HttpResponse("Index Page")
+   return render(request,'blog/index.html')
 def posts(request):
     return HttpResponse("Posts page")
 def post_detail(request):
